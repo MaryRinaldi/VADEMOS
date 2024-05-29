2ee7871ba3b76b68118dfffaefd86f1f36493667
Maria-Carolina-Rinaldi <dr.rinaldimaria@gmail.com>
2024-05-29 12:45:33 +0200

    create routes for db for feedback

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -3,13 +3,18 @@ const router = express.Router();
 const mysql = require('mysql');
 const pool = require('../model/helper');
 
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_NAME = process.env.DB_NAME;
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'your_username',
-    password: 'your_password',
-    database: 'vademos'
-  });
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASS,
+  database: DB_NAME,
+  multipleStatements: true
+});
 
   db.connect(err => {
     if (err) {
